86085c78452e3499b95299a1b3b4c5e6bd7aedae
hoiboidev <hoiboidev@gmail.com>
2022-06-30 19:10:51 +0300

    added back-end

---
+++ back-end/ptl/src/test/java/com/ptl/ptl/PtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptl.ptl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
