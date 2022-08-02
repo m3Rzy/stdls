97f0dc4488538f8ae5578b8389baa91bb2c64a95
hoiboidev <hoiboidev@gmail.com>
2022-08-02 14:15:26 +0300

    version 1.001

--- back-end/myliss/src/main/java/com/myliss/myliss/controller/PupilController.java
+++ back-end/myliss/src/main/java/com/myliss/myliss/controller/PupilController.java
@@ -31,4 +31,5 @@ public class PupilController {
     {
         return service.getPupilById(id);
     }
+
 }
--- main/src/content/journal.js
+++ main/src/content/journal.js
@@ -5,6 +5,7 @@ import '../fonts.css'
 import { Error } from './pages/error' 
 import axios from 'axios'
 
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -12,22 +13,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
 
 
 document.title = "Журнал — myliss";
-// const columns = [
-//   { field: 'id', headerName: 'ID', width: 70, },
-//   { field: 'name', headerName: 'Имя', sortable: false},
-//   { field: 'surname', headerName: 'Фамилия', width: 110, sortable: false},
-//   { field: 'phone', headerName: 'Номер телефона ученика', width: 150, sortable: false},
-//   { field: 'email', headerName: 'Почта ученика', width: 210, sortable: false},
-//   { field: 'cost', headerName: 'Стоимость за урок', width: 150},
-//   { field: 'grade', headerName: 'Класс'},
-//   { field: 'parent', headerName: 'ФИО родителя', sortable: false, width: 210},
-//   { field: 'parent_number', headerName: 'Номер телефона родителя', width: 170, sortable: false},
-//   { field: 'rate', headerName: 'Тариф', sortable: false },
-//   { field: 'button__delete', sortable: false, headerName: ""}
-// ];
+
+
 
 export function Journal() {
     const [pupil, setPupil] = useState(null)
@@ -37,6 +28,9 @@ export function Journal() {
       setPupil(response.data)
     });
   }, [])
+  
+
+  
 
   if (!pupil) return (
     <>
@@ -51,49 +45,53 @@ export function Journal() {
     <h1 className='h1__center'>Журнал</h1>
     <div className='center__table'>
         <div style={{ height: 650, minWidth: '85%', zIndex: 1 }}>
-        <TableContainer component={Paper}>
+        <TableContainer component={Paper} style={{borderRadius: '50'}}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell align="right">Имя</TableCell>
-            <TableCell align="right">Фамилия</TableCell>
-            <TableCell align="right">Номер телефона ученика</TableCell>
-            <TableCell align="right">Почта ученика</TableCell>
-            {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-            <TableCell align="right">Цена</TableCell>
-            <TableCell align="right">Класс</TableCell>
-            <TableCell align="right">ФИО родителя</TableCell>
-            <TableCell align="right">Номер телефона родителя</TableCell>
-            <TableCell align="right">Тариф</TableCell>
-            <TableCell align="right"></TableCell>
+          {/* <TableCell style={{fontFamily: "SFProDisplay-Regular"}}>ID</TableCell > */}
+          <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right" >Имя</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Фамилия</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Номер телефона ученика</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Почта ученика</TableCell >
+            {/* <TableCell  align="right">Protein&nbsp;(g)</TableCell > */}
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Цена</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Класс</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">ФИО родителя</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Номер телефона родителя</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">Тариф</TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right"></TableCell >
+            <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right"></TableCell >
+
           </TableRow>
         </TableHead>
         <TableBody>
           {pupil.map((row) => (
             <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.surname}</TableCell>
-              <TableCell align="right">+7{row.phone}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.cost} ₽</TableCell>
-              <TableCell align="right">{row.grade} класс</TableCell>
-              <TableCell align="right">{row.parent}</TableCell>
-              <TableCell align="right">+7{row.parent_number}</TableCell>
-              <TableCell align="right">{row.rate}</TableCell>
-              <TableCell>
-                  <div className='edit__button'>
-                      <a>?</a>
-                  </div>
-              </TableCell>
-              <TableCell>
+              {/* <TableCell  component="th" scope="row">{row.id}</TableCell > */}
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.name}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.surname}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">+7{row.phone}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.email}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.cost} ₽</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.grade} класс</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.parent}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">+7{row.parent_number}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}} align="right">{row.rate}</TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}}>
+                  <Link to={'/edit/'+row.id}>
+                    <div className='edit__button'>
+                        <a>?</a>
+                    </div>
+                  </Link>
+              </TableCell >
+              <TableCell style={{fontFamily: "SFProDisplay-Regular"}}>
+                <Link to={'/delete/'+row.id}>
                   <div className='remove__button'>
                       <a className='krestik'>╳</a>
                   </div>
-              </TableCell>
+                </Link>
+              </TableCell >
             </TableRow>
           ))}
         </TableBody>
